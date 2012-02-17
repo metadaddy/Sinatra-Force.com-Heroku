0198c04ab3ec6da025ba3281f588aa54060f4df1
Pat Patterson <ppatterson@salesforce.com>
2012-02-16 22:55:25 -0800

    Check we do have a refresh token before trying to refresh

--- demo.rb
+++ demo.rb
@@ -32,7 +32,7 @@ end
 class ForceToken < OAuth2::AccessToken
   def request(verb, path, opts={}, &block)
     response = super(verb, path, opts, &block)
-    if response.status == 401
+    if response.status == 401 && refresh_token
       puts "Refreshing access token"
       @token = refresh!.token
       response = super(verb, path, opts, &block)
